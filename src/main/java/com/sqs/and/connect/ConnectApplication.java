8cb6004e39dccd6343b6b80aa8a64870a757cadf
Natalia Leitnerova <natalia.leitnerova@gmail.com>
2020-08-17 17:35:48 +0200

    sns integration option

--- src/main/java/com/sqs/and/connect/ConnectApplication.java
+++ src/main/java/com/sqs/and/connect/ConnectApplication.java
@@ -1,11 +1,18 @@
 package com.sqs.and.connect;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.PublishRequest;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.http.MediaType;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,25 +23,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ConnectApplication {
 
-    @Autowired private QueueMessagingTemplate queueMessagingTemplate;
+    private List<String> messages = new ArrayList<>();
 
-    @Value("${cloud.aws.end-point.uri}") private String endpoint;
+    @Autowired private QueueMessagingTemplate queueMessagingTemplate;
+    @Autowired private AmazonSNSClient snsClient;
+
+    @Value("${cloud.aws.end-point.sqs}") private String endpoint;
+    @Value("${cloud.aws.end-point.sns-arn}") private String snsArn;
 
     @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
-    public String sendWelcomeMessage() {
+    public String sendWelcomeMessage()
+    {
         return "Go to send/{message} to send a message to the queue.";
     }
 
+
+    /**
+     * Enpoint to send a message directly to AWS SQS
+     *
+     * @param message a message that will be added to the queue
+     * @return text with the message or error message
+     */
     @GetMapping(value = "/send/{message}", produces = MediaType.TEXT_PLAIN_VALUE)
     public String sendMessageToQueue(@PathVariable String message)
     {
-        System.out.println(endpoint);
         try
         {
             queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload(message).build());
         } catch (Exception e)
         {
-            return "Something went wrong - " + e;
+            return "Something went wrong pushing to SQS - " + e;
         }
         return new StringBuilder().append("You message (")
                                   .append(message)
@@ -42,11 +60,52 @@ public class ConnectApplication {
                                   .toString();
     }
 
+    /**
+     * Send message to SNS simpleMessage topic which will pass it onto SQS queue
+     *
+     * @param message a message that will be added to the exchange and to the queue
+     * @return text with the message or error message
+     */
+    @GetMapping("/sendMessage/{message}")
+    public String publishMessageToTopic(@PathVariable String message)
+    {
+        try
+        {
+            PublishRequest publishRequest = new PublishRequest(snsArn, message, "New message");
+            snsClient.publish(publishRequest);
+        } catch (Exception e)
+        {
+            return "Something went wrong pushing to SNS simple Message topic - " + e;
+        }
 
-    //@SqsListener("RS-SQS-standard-queue")
-    //public void loadMessageFromSQS(String message)  {
-    //	logger.info("message from SQS Queue {}", message);
-    //}
+        return new StringBuilder().append("You message (").append(message).append(
+                ") was sent to the simpleMessage SNS topic.").toString();
+    }
+
+
+    @SqsListener("RS-SQS-standard-queue")
+    public void loadMessageFromSQS(String message)
+    {
+        try
+        {
+            JSONObject outputJson = new JSONObject(message);
+            messages.add("SNS->SQS: " + outputJson.get("Message").toString());
+        } catch (Exception e)
+        {
+            messages.add("SQS: " + message);
+        }
+    }
+
+    /**
+     * Shows all messages that have been polled from the queue
+     *
+     * @return list of messages
+     */
+    @GetMapping("/read")
+    public String readMessages()
+    {
+        return messages.toString();
+    }
 
 
     public static void main(String[] args)
--- src/main/java/com/sqs/and/connect/config/AwsConfig.java
+++ src/main/java/com/sqs/and/connect/config/AwsConfig.java
@@ -3,16 +3,19 @@ package com.sqs.and.connect.config;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
-public class AwsSQSConfig {
+public class AwsConfig {
 
     @Value("${cloud.aws.region.static}") private String region;
 
@@ -30,11 +33,24 @@ public class AwsSQSConfig {
     @Bean
     public AmazonSQSAsync amazonSQSAsync()
     {
-        return AmazonSQSAsyncClientBuilder.standard().withRegion(Regions.AP_SOUTH_1)
+        return AmazonSQSAsyncClientBuilder.standard()
+                                          .withRegion(Regions.US_EAST_2)
                                           .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(
                                                   awsAccessKey,
                                                   awsSecretKey)))
                                           .build();
+    }
+
+    @Primary
+    @Bean
+    public AmazonSNSClient getSnsClient()
+    {
+        return (AmazonSNSClient) AmazonSNSClientBuilder.standard()
+                                                       .withRegion(Regions.US_EAST_2)
+                                                       .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(
+                                                               awsAccessKey,
+                                                               awsSecretKey)))
+                                                       .build();
     }
 
 }
